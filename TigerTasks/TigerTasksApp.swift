12bc30ad09d0572e6703512c42bf19f4c116777d
noahthompson <net0016@auburn.edu>
2023-11-05 20:16:00 -0600

    Initial Commit

---
+++ TigerTasks/TigerTasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TigerTasksApp.swift
+//  TigerTasks
+//
+//  Created by Noah Thompson on 11/5/23.
+//
+
+import SwiftUI
+
+@main
+struct TigerTasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
